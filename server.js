b1ed8d5481b82a3477001ecde1577617bdb5f856
kharchev <kharchevbv@gmail.com>
2022-04-30 21:37:11 +0300

    fixed express port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = process.env.PORT || 5001;
+const port = 5001;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 app.use(express.static("dist"));
